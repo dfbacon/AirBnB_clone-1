856fc61cacf217a12863e6c7e2a1757e1f2b9b84
Daniel Bacon <dbacon338@gmail.com>
2017-03-22 17:55:19 +0000

    changed tabs

--- models/amenity.py
+++ models/amenity.py
@@ -9,6 +9,8 @@ class Amenity(BaseModel, Base):
     if os.environ['HBNB_TYPE_STORAGE'] == "db":
         __tablename__ = "amenities"
         name = Column(String(128), nullable=False)
+    else:
+        name = ""
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
