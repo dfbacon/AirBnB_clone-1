422e9cfd1c8f48c0d693ed8d4fd58aa943e08897
Daniel Bacon <dbacon338@gmail.com>
2017-03-22 20:04:29 +0000

    updates and additions to create db storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,27 +1,62 @@
-from models import *
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+#!/usr/bin/python3
+'''
+This is the 'db_storage' module
+'''
+import sys
 import os
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from models.base_model import Base
+from sqlalchemy import inspect
 
 
 class DBStorage():
-    """ """
+    """This is the 'DBStorage class"""
     __engine = None
     __session = None
+    valid_models = ["User", "State", "City", "Amenity", "Place", "Review"]
 
-    def init(self):
-        self.uname = os.environment["HBNB_MYSQL_USER"]
-        self.upass = os.environment["HBNB_MYSQL_PWD"]
-        self.host = os.environment["HBNB_MYSQL_HOST"]
-        self.dbname = os.environment["HBNB_MYSQL_DB"]
+    def __init__(self):
+        '''This is the initialization method'''
+        uname = os.environ["HBNB_MYSQL_USER"]
+        upass = os.environ["HBNB_MYSQL_PWD"]
+        host = os.environ["HBNB_MYSQL_HOST"]
+        dbname = os.environ["HBNB_MYSQL_DB"]
         self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}"
-                                      .format(self.uname, self.upass,
-                                              self.host, self.dbname))
-
+                                      .format(uname, upass, host, dbname))
         self.__Session = sessionmaker()
         self.__Session.configure(bind=self.__engine)
         Base.metadata.create_all(self.__engine)
         self.__session = self.__Session()
 
     def all(self, cls=None):
-        
+        '''This is the 'all' method'''
+        query = {}
+        if cls is not None:
+            for instance in self.__session.query(cls):
+                query.update(instance.id, instance)
+            return (query)
+        else:
+            for cls in valid_models:
+                cls = getattr(sys.modules["models"], cls)
+                for instance in self.__session.query(cls):
+                    query.update({instance.id: instance})
+            return (query)
+
+    def new(self, obj):
+        '''This is the 'new' method'''
+        self.__session.add(obj)
+
+    def save(self):
+        '''This is the 'save' method'''
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        '''This is the 'delete' method'''
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        '''This is the 'reload' method'''
+        Base.metadata.create_all(self.__engine)
+        self.__session = self.__Session()
