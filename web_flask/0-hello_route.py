afdb271ede16208c323084a92fcead41315512c0
Daniel Bacon <dbacon338@gmail.com>
2017-03-28 18:51:17 +0000

    basic flask setup

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,21 @@
+#/usr/bin/python3
+'''
+This is the '0-hello_route' module.
+
+0-hello_route utilizes Flask to start a web application:
+* listening on 0.0.0.0,
+* port 5000,
+* displays "Hello HBNB!"
+'''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return ('Hello HBNB!')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
