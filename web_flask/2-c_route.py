e225aaad6599e44d6dc2a94c1b3a375c8601e89c
Daniel Bacon <dbacon338@gmail.com>
2017-03-28 20:52:19 +0000

    start flask web app

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,34 @@
+#/usr/bin/python3
+'''
+This is the '2-c_route' module.
+
+2-c_route utilizes Flask to start a web application:
+* listening on 0.0.0.0,
+* port 5000,
+* (/) displays "Hello HBNB!"
+* (/hbnb) displays "HBNB"
+* (/c/<text>) displays "c text"
+'''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return ('Hello HBNB!')
+
+
+@app.route('/hbnb')
+def hello_hbnb():
+    return ('HBNB')
+
+
+@app.route('/c/<text>')
+def hello_c(text):
+    txt = text.replace('_', ' ')
+    return ('c {}'.format(txt))
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
