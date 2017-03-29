3ab1d2dee74aaaf9d7c7132eb19236dfb7cf7fac
Daniel Bacon <dbacon338@gmail.com>
2017-03-29 18:09:04 +0000

    flask integration with sqlalchemy

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+This is the '7-states_list' module.
+
+7-states_list utilizes Flask to start a web application:
+* listening on 0.0.0.0
+* port 5000
+* Integrage with sqlalchemy
+'''
+from models import storage
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/states_list/')
+def state_list():
+    states = storage.all("State")
+    return (render_template("7-states_list.html", states=states))
+
+
+@app.teardown_appcontext
+def close_out(self):
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
