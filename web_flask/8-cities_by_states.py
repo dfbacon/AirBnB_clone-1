ebd1ed46461e9c67a9d03f621e6d5571136e16c7
Daniel Bacon <dbacon338@gmail.com>
2017-03-30 19:41:58 +0000

    edits to flask setup

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -8,20 +8,49 @@ This is the '8-cities_by_state' module.
 * Integrage with sqlalchemy
 '''
 from models import storage
+from models.base_model import Base
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 from flask import Flask
 from flask import render_template
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from os import getenv
+
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states')
-def state_list():
-    states = storage.all("State")
-    return (render_template("8-cities_by_states.html", states=states))
+def cities_by_statess():
+    """List all cities by states"""
+    states = storage.all("State").values()
+    cities = storage.all("City").values()
+    result = [[state, [city for city in cities if city.state_id == state.id]]
+              for state in states]
+    result.sort(key=lambda x: x[0].name)
+    return render_template("8-cities_by_states.html",
+                           Query_name="States", result=result)
+
+
+@app.route('/cities_by_states/')
+def cities_by_states():
+    """List all cities by states"""
+    states = storage.all("State").values()
+    result = []
+    for state in sorted(states, key=lambda x: x.name):
+        result.append([state, state.cities])
+    return render_template("8-cities_by_states.html",
+                           Query_name="States", result=result)
 
 
 @app.teardown_appcontext
-def teardown(exception):
+def close_session(exception):
+    """Remove the db session or save file"""
     storage.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
