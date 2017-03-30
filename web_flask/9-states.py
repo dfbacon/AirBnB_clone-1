839c9e3e65fa5e78f2830a38d326f555959d6482
Daniel Bacon <dbacon338@gmail.com>
2017-03-30 20:44:27 +0000

    flask web app setup

---
+++ web_flask/9-states.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+'''
+This is the '9-states' module.
+
+9-states utilizes Flask to start a web application:
+* listening on 0.0.0.0
+* port 5000
+* Integrage with sqlalchemy
+'''
+from models import storage
+from models.base_model import Base
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from flask import Flask
+from flask import render_template
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from os import getenv
+
+app = Flask(__name__)
+
+
+@app.route('/states/')
+@app.route('/states/<id_number>')
+def cities_by_states(id_number="all"):
+    """List all cities by states"""
+    states = storage.all("State")
+    if id_number == "all":
+        return(render_template("9-states.html", state="all",
+                               Query_name="States", states=states.values()))
+    else:
+        flag = False
+        for key, value in states.items():
+            if key == id_name:
+                flag = True
+                break
+        if flag:
+            found = value.cities
+            return(render_template("9-states.html", state="1",
+                                   Query_name="State: {}".format(value.name),
+                                   states=found))
+        else:
+            return(render_template("9-states.html", state="",
+                                   Query_name="None", states=states))
+
+
+@app.teardown_appcontext
+def close_session(exception):
+    """Remove the db session or save file"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
