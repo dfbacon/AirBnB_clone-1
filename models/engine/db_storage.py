581f8b2297071d36f6192d574b81bbafcc0747b9
Daniel Bacon <dbacon338@gmail.com>
2017-03-30 17:03:26 +0000

    overhaul db engine

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,59 +2,49 @@
 '''
 This is the 'db_storage' module
 '''
-import sys
-import os
-from models.base_model import BaseModel, Base
+from models.base_model import Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import (sessionmaker, scoped_session)
+from os import getenv
 from models.user import User
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-from sqlalchemy.orm.scoping import scoped_session
 
 
 class DBStorage():
     """This is the 'DBStorage class"""
     __engine = None
     __session = None
-    valid_models = {"Amenity": Amenity, "City": City, "State": State,
-                     "Place": Place, "Review": Review,
-                     "User": User, "PlaceAmenity": PlaceAmenity}
+    __Session = None
 
     def __init__(self):
         '''This is the initialization method'''
-        usr = os.getenv('HBNB_MYSQL_USER')
-        pwd = os.getenv('HBNB_MYSQL_PWD')
-        host = os.getenv('HBNB_MYSQL_HOST')
-        db = os.getenv('HBNB_MYSQL_DB')
-        env = os.getenv('HBNB_MYSQL_ENV')
-
-        eng_str = "mysql+mysqldb://{}:{}@{}/{}".format(usr, pwd, host, db)
-
-        self.__engine = create_engine(eng_str)
-        Session = sessionmaker(bind=self.__engine)
-        Base.metadata.create_all(self.__engine)
-        self.__session = Session()
-        if env == 'test':
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+            getenv('HBNB_MYSQL_USER'),
+            getenv('HBNB_MYSQL_PWD'),
+            getenv('HBNB_MYSQL_HOST'),
+            getenv('HBNB_MYSQL_DB')))
+        self.__models_available = {"User": User, "Amenity": Amenity,
+                                   "City": City, "Place": Place,
+                                   "Review": Review, "State": State}
+        if getenv('HBNB_MYSQL_ENV', 'not') == 'test':
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        query_dict = {}
-        if cls is None:
-            for val_key, val_cls in self.valid_models.items():
-                for instance in self.__session.query(val_cls):
-                    query_dict[instance.id] = instance
+        objects = {}
+        if cls:
+            for key in self.__session.query(self.__models_available[cls]):
+                objects[key.__dict__['id']] = key
         else:
-            if (isinstance(cls, str) is True):
-                for instance in self.__session.query(eval(cls)):
-                    query_dict[instance.id] = instance
-            else:
-                for instance in self.__session.query(cls):
-                    query_dict[instance.id] = instance
-        return (query_dict)
+            for value in self.__models_available.values():
+                i = self.__session.query(value).all()
+                if i:
+                    for key in i:
+                        objects[key.__dict__['id']] = key
+        return (objects)
 
     def new(self, obj):
         '''This is the 'new' method'''
@@ -71,8 +61,8 @@ class DBStorage():
 
     def reload(self):
         '''This is the 'reload' method'''
-        self.__session = scoped_session(sessionmaker(bind=self.__engine))
         Base.metadata.create_all(self.__engine)
+        self.__session = scoped_session(sessionmaker(bind=self.__engine))
 
     def close(self):
         '''this si the 'close' method
